c304eb48df8e51302bd5eb013dbe756fe55ee4a3
PCJdvp <PCJdvp@DESKTOP-0HPBT0I>
2018-03-03 15:13:11 -0500

    Agregar archivos de proyecto.

---
+++ cms/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace cms
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      // CSS ADMIN LTE
+                      "~/admin-lte/css/AdminLTE.css",
+                      //"~/Content/css/font-awesome.css", CDN
+                      "~/admin-lte/css/skins/skin-blue.css",
+                      "~/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"
+                      ));
+            // PARA ADMIN LTE
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+                         "~/admin-lte/js/adminlte.js",
+                         "~/admin-lte/plugins/fastclick/fastclick.js",
+                         "~/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"
+                         ));
+        }
+    }
+}
---
+++ cms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cms.Startup))]
+namespace cms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
